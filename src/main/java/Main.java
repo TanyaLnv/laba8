0c7b90753ec79b6cba9ba909cb92327a60a64b01
Romanovich Valerya <romanovich.valeriya@inbox.ru>
2022-11-16 19:32:23 +0300

    added functions 7-12

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,77 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList();
+        StrFunctions strFunctions = new StrFunctions();
+        Scanner scan = new Scanner(System.in);
+        int i;
+        boolean check = true;
 
+        System.out.println("Введите максимальную размерность при статическом добавлении элементов:");
+        int maxsize = scan.nextInt();
+
+        while(check) {
+            System.out.println("Выберите действие");
+            i = scan.nextInt();
+            switch (i) {
+                case 1:
+                    break;
+                case 2:
+                    break;
+                case 3:
+                    break;
+                case 4:
+                    break;
+                case 5:
+                    break;
+                case 6:
+                    break;
+                case 7:
+                    System.out.print("Введите подстроку:");
+                    scan.nextLine();
+                    String substring = scan.nextLine();
+                    strFunctions.printList(strFunctions.findItemsWithSubstring(list, substring));
+                    break;
+                case 8:
+                    System.out.print("Введите путь к файлу:");
+                    scan.nextLine();
+                    String pathToFile = scan.nextLine();
+                    strFunctions.initializeListFromTextFile(pathToFile);
+                    break;
+                case 9:
+                    System.out.print("Введите индексы объектов для сравнения:");
+                    int firstItemIndex = scan.nextInt();
+                    int secondItemIndex = scan.nextInt();
+                    if(strFunctions.compareInnerObjects(list, firstItemIndex, secondItemIndex)){
+                        System.out.println("Строки одинаковые");
+                    } else {
+                        System.out.println("Строки разные");
+                    }
+                    break;
+                case 10:
+                    for(var length : strFunctions.itemLengthList(list)){
+                        System.out.println(length);
+                    }
+                    break;
+                case 11:
+                    System.out.print("Введите строку для статического добавления:");
+                    scan.nextLine();
+                    String str = scan.nextLine();
+                    strFunctions.addItemStatic(list, str, maxsize);
+                    break;
+                case 12:
+                    for(int j =0; j< list.size(); j++ )
+                    {
+                        System.out.println(list.get(j));
+                    }
+                    break;
+                case 0: check = false;
+                    break;
+                default:
+                    System.out.println("Такого действия нет. Повторите ввод");
+            }
+        }
     }
 }
--- src/main/java/StrFunctions.java
+++ src/main/java/StrFunctions.java
@@ -1,2 +1,77 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class StrFunctions {
+    public ArrayList<String> findItemsWithSubstring(ArrayList<String> list, String substring){
+        ArrayList<String> foundItems = new ArrayList<>();
+        for (String item : list) {
+            if(item.contains(substring)){
+                foundItems.add(item);
+            }
+        }
+        return foundItems;
+    }
+
+    public ArrayList<String> initializeListFromTextFile(String pathToTextFile){
+        var fileLinesList = new ArrayList<String>();
+        try(var fileReader = new BufferedReader(new FileReader(pathToTextFile))){
+            String line = fileReader.readLine();
+            while(line != null){
+                fileLinesList.add(line);
+                line = fileReader.readLine();
+            }
+        }catch (FileNotFoundException e) {
+            System.out.println("File wasn't found.");
+        } catch (IOException e) {
+            System.out.println("IOException");
+        }
+        printList(fileLinesList);
+        return fileLinesList;
+    }
+
+    public void printList(ArrayList<String> collection){
+        if(collection.size() == 0){
+            System.out.println("Collection is empty.");
+        } else {
+            System.out.println("Collection items:");
+            for(var item : collection){
+                System.out.println(item);
+            }
+        }
+    }
+
+    public boolean compareInnerObjects(ArrayList<String> list, int firstItemIndex, int secondItemIndex){
+//        if(firstObjectIndex < 0 || secondObjectIndex < 0){
+//            return false;
+//        }
+        String firstItem = list.get(firstItemIndex).trim();
+        String secondItem = list.get(secondItemIndex).trim();
+        return firstItem.equals(secondItem);
+    }
+
+    public ArrayList<Integer> itemLengthList(ArrayList<String> list){
+        var itemLengthList = new ArrayList<Integer>();
+        for(var item : list){
+            itemLengthList.add(item.length());
+        }
+        Collections.sort(itemLengthList);
+        return itemLengthList;
+    }
+
+    public ArrayList<String> addItemStatic(ArrayList<String> list, String itemToAdd, int maxsize){
+        //если
+        if(maxsize < 0){
+            //throw new InvalidCollectionSizeException();
+            return list;
+        }
+        while(list.size() >= maxsize){
+            list.remove(0);
+        }
+        list.add(itemToAdd);
+        return list;
+    }
 }
