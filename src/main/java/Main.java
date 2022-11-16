7b4ab7bebe7fb48b48c904eaeadc0512481e4b8d
Tanya <tanya.leonova.24012003@gmail.com>
2022-11-16 19:29:59 +0300

    functions 1-5

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,70 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList();
+        StrFunctions strFunctions = new StrFunctions();
+        Scanner scan = new Scanner(System.in);
+        int i;
+        boolean check = true;
+        while(check) {
+            System.out.println("Выберите действие");
+            System.out.println(" 1 - Добавление\n" +
+                    "2 - Удаление\n" +
+                    "3 - Поиск одинаковых элементов\n" +
+                    "4 - Выгрузка\n" +
+                    "5 - Реверс\n" +
+                    "6 - Статистика символов\n" +
+                    "7 - Поиск подстроки\n" +
+                    "8 - Инициализация\n" +
+                    "9 - Сравнение элементов\n" +
+                    "10 - Длина строк\n" +
+                    "11 - Статическое добавление\n"+
+                    "12 - Вывод на экран\n"+
+                    "0 - Выход\n");
+            i = scan.nextInt();
+            switch (i) {
+                case 1: strFunctions.add(list);
+                    break;
+                case 2:
+                    strFunctions.remove(list);
+                    break;
+                case 3:
+                    strFunctions.findEqual(list);
+                    break;
+                case 4:
+                    strFunctions.toXML(list);
+                    break;
+                case 5:
+                    strFunctions.reverse(list);
+                    break;
+                case 6:
+                    strFunctions.statistics(list);
+                    break;
+                case 7:
 
+                    break;
+                case 8:
+                    break;
+                case 9:
+                    break;
+                case 10:
+                    break;
+                case 11:
+                    break;
+                case 12:
+                    System.out.println("Ваш массив:");
+                    for(int j =0; j< list.size(); j++ )
+                    {
+                        System.out.println(list.get(j));
+                    }
+                    break;
+                case 0: check = false;
+                    break;
+                default:
+                    System.out.println("Такого действия нет. Повторите ввод");
+            }
+        }
     }
 }
--- src/main/java/StrFunctions.java
+++ src/main/java/StrFunctions.java
@@ -1,2 +1,112 @@
+import java.beans.XMLEncoder;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.*;
+
 public class StrFunctions {
+    Scanner scan = new Scanner(System.in);
+
+    public void add(ArrayList<String> list){
+        System.out.println("Введите слово для добавления: ");
+        String str;
+        str = scan.nextLine();
+        list.add(str);
+    }
+
+    public void remove(ArrayList<String> list)
+    {
+        if(list.isEmpty()){
+            System.out.println("Пусто");
+        }else {
+            System.out.println("Введите слово для удаления (происходит удаление первого вхождения слова, если есть одиноковые слова, повторите процедуру удаления )");
+            String str;
+            str = scan.nextLine();
+            if(list.remove(str)){
+                System.out.println("Объект успешно удален");
+            }
+            else{
+                System.out.println("Такого объекта в списке нет");
+            }
+        }
+    }
+
+    public void findEqual(ArrayList<String> list) {
+        HashMap<String, Integer> hm= new HashMap<>();
+        for(int i=0; i< list.size(); i++)
+        {
+            if(hm.containsKey(list.get(i))){
+                hm.replace(list.get(i), hm.get(list.get(i))+1);
+            }
+            else {
+                hm.put(list.get(i), 1);
+            }
+        }
+        SortedMap<String, Integer> sm = new TreeMap<>(hm);
+        for(Map.Entry<String, Integer> pair : sm.entrySet() )
+        {
+            if(pair.getValue()!=1) {
+                System.out.println(pair.getKey() + " " + pair.getValue());
+            }
+        }
+    }
+
+    public void toXML (ArrayList < String > list) {
+        FileOutputStream fos = null;
+        XMLEncoder encoder = null;
+        try {
+            fos = new FileOutputStream("array.xml");
+            encoder = new XMLEncoder(fos);
+            for(int i=0;i<list.size(); i++){
+                encoder.writeObject(list.get(i));
+            }
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+        finally {
+            if(encoder!=null)
+                encoder.close();
+            try {
+                if(fos!=null)
+                    fos.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void reverse (ArrayList < String > list)
+    {
+        String sb;
+        for (int i = 0; i < list.size(); i++) {
+            sb = new StringBuilder(list.get(i)).reverse().toString();
+            list.remove(i);
+            list.add(i, sb);
+        }
+    }
+
+    public void statistics (ArrayList < String > list) {
+        StringBuilder newList = new StringBuilder();
+        for(int i=0;i<list.size(); i++){
+            newList.append(list.get(i));
+        }
+        String str = newList.toString();
+        char [] chars = str.toCharArray();
+        HashMap<Character, Integer> hm= new HashMap<>();
+        for(int i=0; i< chars.length; i++)
+        {
+            if(hm.containsKey(chars[i])){
+                hm.replace(chars[i], hm.get(chars[i])+1);
+            }
+            else {
+                hm.put(chars[i], 1);
+            }
+        }
+        SortedMap<Character, Integer> sm = new TreeMap<>(hm);
+        for(Map.Entry<Character, Integer> pair : sm.entrySet() )
+        {
+            System.out.println(pair.getKey() + " " + pair.getValue());
+        }
+    }
+
 }
